c6a191fd8a24a1841e3b88dba268affc42479fcc
alcatdra <alexander.menko.reggie@gmail.com>
2022-12-02 13:48:39 +0300

    added more bells to playground scene (yet to test)

--- Assets/Scripts/AudioTrigger.cs
+++ Assets/Scripts/AudioTrigger.cs
@@ -6,19 +6,17 @@ using UnityEngine;
 public class AudioTrigger : MonoBehaviour
 {
     public AudioSource audioSource;
-    void OnCollisionEnter(Collision collision){
+    void OnCollisionEnter(Collision collision)
+    {
+        //if (collision.gameObject.name == "audio trigger")
+        //{
         audioSource.PlayOneShot(audioSource.clip, volumeBasedOnMagnitude(collision));
         Debug.Log(collision.relativeVelocity.magnitude);
+        //}
     } 
 
     float volumeBasedOnMagnitude(Collision collision)
     {
-        /*old code   
-        if (collision.relativeVelocity.magnitude > 2)
-        return 1f;
-        else if (collision.relativeVelocity.magnitude > 1 && collision.relativeVelocity.magnitude < 2)
-        return 0.5f;
-        else return 0.1f; */
         if (collision.relativeVelocity.magnitude > 3)
         return 1f;
         else return collision.relativeVelocity.magnitude / 3f;
